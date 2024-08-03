e6670b3b1bb9c2489d312443f026ffbf7c6c2294
Nexidict <nexidict.developer@gmail.com>
2024-08-03 19:12:22 +0200

    Revamped UI again + Dynamic layout Finally managed to adjust sizes correctly, adding the effects and interactions panel in there. Temporary removal of admin checks due to issues when it comes to updating the window

--- interface/scripted/starpounds/curses/curses.lua
+++ interface/scripted/starpounds/curses/curses.lua
@@ -10,8 +10,6 @@ function init()
 
     isAdmin = admin()
 
-    weightDecrease:setVisible(isAdmin)
-    weightIncrease:setVisible(isAdmin)
     enableControls = metagui.inputData.isObject or isAdmin
     selectedCurse = nil
 
@@ -23,12 +21,7 @@ function init()
 end
 
 function update()
-    if isAdmin ~= admin() then
-        isAdmin = admin()
-        weightDecrease:setVisible(isAdmin)
-        weightIncrease:setVisible(isAdmin)
-        barPadding:setVisible(not isAdmin)
-    end
+    
 end
 
 function buildTabs()
@@ -36,8 +29,8 @@ function buildTabs()
         tab.title = " "
         tab.icon = string.format("icons/tabs/%s.png", tab.id)
         tab.contents = copy(tabField.data.templateTab)
-        tab.contents[2].children[2].text = tab.friendlyName
-        tab.contents[2].children[4].children[1].id = string.format("panel_%s", tab.id)
+        tab.contents[1].children[2].text = tab.friendlyName
+        tab.contents[1].children[3].children[1].id = string.format("panel_%s", tab.id)
 
         local newTab = tabField:newTab(tab)
 
@@ -60,7 +53,7 @@ end
 
 function makeCurseWidget(curseKey, curse)
     local curseWidget = {
-        type = "layout", size = {145, 20}, mode = "manual", children = {
+        type = "layout", size = {142, 20}, mode = "manual", children = {
             { id = string.format("%sCurse_back", curseKey), type = "image", noAutoCrop = true, position = {0, 0}, file = "item.png" },
             { id = string.format("%sCurse_name", curseKey), type = "label", position = {22, 6}, size = {117, 9}, text = curse.friendlyName },
             { id = string.format("%sCurse_icon", curseKey), type = "image", noAutoCrop = true, position = {2, 2}, file =  "icons/curses/placeholder.png" }
@@ -88,60 +81,6 @@ function tabField:onTabChanged(tab, previous)
     end
 end
 
-function weightDecrease:onClick()
-    local progress = (starPounds.weight - starPounds.currentSize.weight)/((starPounds.sizes[starPounds.currentSizeIndex + 1] and starPounds.sizes[starPounds.currentSizeIndex + 1].weight or starPounds.settings.maxWeight) - starPounds.currentSize.weight)
-    local targetWeight = starPounds.sizes[math.max(starPounds.currentSizeIndex - 1, 1)].weight
-    local targetWeight2 = starPounds.sizes[starPounds.currentSizeIndex].weight
-    starPounds.setWeight(metagui.checkShift() and 0 or (targetWeight + (targetWeight2 - targetWeight) * progress))
-end
-
-function weightIncrease:onClick()
-    local progress = math.max(0.01, (starPounds.weight - starPounds.currentSize.weight)/((starPounds.sizes[starPounds.currentSizeIndex + 1] and starPounds.sizes[starPounds.currentSizeIndex + 1].weight or starPounds.settings.maxWeight) - starPounds.currentSize.weight))
-    local targetWeight = starPounds.sizes[starPounds.currentSizeIndex + 1] and starPounds.sizes[starPounds.currentSizeIndex + 1].weight or starPounds.settings.maxWeight
-    local targetWeight2 = starPounds.sizes[starPounds.currentSizeIndex + 2] and starPounds.sizes[starPounds.currentSizeIndex + 2].weight or starPounds.settings.maxWeight
-    starPounds.setWeight(metagui.checkShift() and starPounds.settings.maxWeight or (targetWeight + (targetWeight2 - targetWeight) * progress))
-end
-
-function enable:onClick()
-    local buttonIcon = string.format("%s.png", starPounds.toggleEnable() and "enabled" or "disabled")
-    enable:setImage(buttonIcon, buttonIcon, buttonIcon.."?border=2;00000000;00000000?crop=2;3;88;22")
-end
-
-function reset:onClick()
-    local confirmLayout = sb.jsonMerge(root.assetJson("/interface/confirmation/resetstarpoundsconfirmation.config"), {
-        title = "Curses",
-        icon = "/interface/scripted/starpounds/curses/icon.png",
-        images = { portrait = world.entityPortrait(player.id(), "full") }
-    })
-
-    promises:add(
-        player.confirm(confirmLayout), 
-        function(response)
-            if response then
-                promises:add(world.sendEntityMessage(player.id(), "starPounds.reset"), 
-                    function()
-                        local buttonIcon = "disabled.png"
-                        enable:setImage(buttonIcon, buttonIcon, buttonIcon.."?border=2;00000000;00000000?crop=2;3;88;22")
-                    end
-                )
-            end
-        end
-    )
-end
-
 function admin()
     return (player.isAdmin() or starPounds.hasOption("admin")) or false
-end
-
-function replaceInData(data, keyname, value, replacevalue)
-    if type(data) == "table" then
-        for k, v in pairs(data) do
-            if (k == keyname or keyname == nil) and (v == value or value == nil) then
-                -- sb.logInfo("Replacing value %s of key %s with value %s", v, k, replacevalue)
-                data[k] = replacevalue
-            else
-                replaceInData(v, keyname, value, replacevalue)
-            end
-        end
-    end
 end
